b1fe8b3b20cea019cbb37952b5f5a7fa508942b0
yd499398-dot <yd499398@gmail.com>
2026-07-25 17:36:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/cyberguard-website/',
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    },
-  },
-});
+export default defineConfig(({ mode }) => {
+  // Check if we are running in Vercel's build environment
+  const isVercel = process.env.VERCEL;
+
+  return {
+    plugins: [react()],
+    // If Vercel, serve from root. Otherwise, serve from the GitHub repo path.
+    base: isVercel ? '/' : '/cyberguard-website/',
+  }
+})
